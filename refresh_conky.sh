5b401edae439db7206583260488921e768938938
xexpanderx <andrei.alexsson@gmail.com>
2020-04-04 16:13:25 +0200

    Match *color at beginning of each line

--- refresh_conky.sh
+++ refresh_conky.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-colors=`cat $HOME/.cache/wal/colors.Xresources | grep "*color" | tr -d "*:" | sed 's/ //g' | sed 's/#/="#/g' | sed ':a;N;$!ba;s/\n/"\n/g'`
+colors=`cat $HOME/.cache/wal/colors.Xresources | grep "^*color" | tr -d "*:" | sed 's/ //g' | sed 's/#/="#/g' | sed ':a;N;$!ba;s/\n/"\n/g'`
 colors="${colors}\""
 cat ~/.conky/conky-drawer-interactive/templates/drawer_template.lua | awk -v srch="COLORFIELD" -v repl="$colors" '{ sub(srch,repl,$0); print $0 }' > ~/.conky/conky-drawer-interactive/lua/drawer.lua
